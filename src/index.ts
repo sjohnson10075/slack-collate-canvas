77e00c22224d586a843fb78e05b0a1b76f6b137c
sjohnson10075 <sirena@corporatebdsolutions.com>
2026-06-15 00:10:59 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -529,6 +529,8 @@ console.log("JPEG COMPRESS DONE", {
 
   const pdfBytes = await pdf.save();
   const bodyBuf = Buffer.from(pdfBytes);
+
+  // STEP 4 will be added here later
 }
 
 async function compressToJpeg(buf: Buffer, max: number): Promise<Buffer> {
