8cd566da864b7ee108e843d5580a467956eb6100
sjohnson10075 <sirena@corporatebdsolutions.com>
2026-08-17 18:19:06 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -97,7 +97,8 @@ const CHANNEL_TO_CLIENT_OPTION_GID: Record<string, string> = {
   "C0BDUAEFC80": "1215990021196418", // 328 Ridgewood_MV_Davis
   "C0BBZS5K3B6": "1215848663290805", // 2179 Yaokim_Sullivan
   "C0BF0TDELKY": "1216464210126671", // 59 Barn View Ct_McKible
-  "C0BM4TPUR8U": "1217040214655966" // 335 Golden Gate Ave_Belvedere
+  "C0BM4TPUR8U": "1217040214655966", // 335 Golden Gate Ave_Belvedere
+  "C0BQRF469NZ": "1217557943763917" // 303 Jester Ct_Lefebvre
 };
 
 function verifySlackSig(req: express.Request): boolean {
